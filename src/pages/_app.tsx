a974da59867584dc70ec7881b71ed864baf7b795
AlejandroYanes <alejandro.yanes94@gmail.com>
2022-10-30 00:28:41 +0100

    added light/dark styles based on system preferences

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,23 +2,35 @@ import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { MantineProvider } from '@mantine/core';
-
-import { trpc } from "../utils/trpc";
-
+import { useColorScheme } from '@mantine/hooks';
+import { trpc } from "utils/trpc";
 import "../styles/globals.css";
 
 const MyApp: AppType<{ session: Session | null }> = ({
-  Component,
-  pageProps: { session, ...pageProps },
-}) => {
+                                                       Component,
+                                                       pageProps: { session, ...pageProps },
+                                                     }) => {
+  const colorScheme = useColorScheme();
   return (
     <SessionProvider session={session}>
       <MantineProvider
         withGlobalStyles
         withNormalizeCSS
         theme={{
-          /** Put your mantine theme override here */
-          colorScheme: 'light',
+          colorScheme,
+          globalStyles: (theme) => ({
+            '*, *::before, *::after': {
+              boxSizing: 'border-box',
+            },
+            html: {
+              colorScheme: theme.colorScheme,
+            },
+            body: {
+              ...theme.fn.fontStyles(),
+              backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.white,
+              color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.black,
+            },
+          }),
         }}
       >
         <Component {...pageProps} />
