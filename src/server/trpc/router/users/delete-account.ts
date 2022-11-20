f564e79cf55f7f1dd498260b211fcfb64d048870
AlejandroYanes <alejandro.yanes94@gmail.com>
2022-11-20 21:35:47 +0000

    added a logic to delete the user data from Novu when deleting the account

--- src/server/trpc/router/users/delete-account.ts
+++ src/server/trpc/router/users/delete-account.ts
@@ -1,10 +1,13 @@
-import { protectedProcedure } from '../../trpc';
+import { protectedProcedure } from 'server/trpc/trpc';
+import novu from 'server/novu/_novu';
 
 const deleteAccount = protectedProcedure
   .mutation(async ({ ctx }) => {
+    const { session: { user } } = ctx;
+    await novu.subscribers.delete(user.id);
     return ctx.prisma.user.delete({
       where: {
-        id: ctx.session.user.id,
+        id: user.id,
       },
     });
   });
